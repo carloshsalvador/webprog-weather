3424361edc919cb9e4b2a447760f39c96187a0db
Erik Behrends <behrends@users.noreply.github.com>
2023-01-08 18:29:23 +0100

    Wetterlage aus anhand Wettercodes ableiten

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ function addLocation(name, temp, condition, image) {
   new_location.innerHTML = `<div class="location">${name}</div>
     <div class="weather_info">
       <span>
-        <span class="temperature">${temp} Grad</span>
+        <span class="temperature">${temp}°</span>
         <span class="condition">${condition}</span>
       </span>
       <img
@@ -50,14 +50,14 @@ async function doSearch() {
   }
 
   // TODO: Wetter-Daten für location laden
-  const temp = await loadWeatherData(
+  const { temp, weathercode } = await loadWeatherData(
     location.latitude,
     location.longitude
   );
 
-  // TODO: condition und image korrekt laden
+  const condition = weatherCodes.get(weathercode);
+  // TODO: image korrekt laden
   // TODO: doppelte Orte vermeiden?
-  const condition = 'heiter';
   const image =
     'https://cdn.glitch.me/c569e324-22c3-491c-ab27-94a3498d6207%2Fsun-cloudy-line.png?v=1634724998045';
   addLocation(location.name, temp, condition, image);
@@ -68,14 +68,14 @@ async function doAddFromList() {
   const location_index = selected_location.value;
   const location = locations[location_index];
 
-  const temp = await loadWeatherData(
+  const { temp, weathercode } = await loadWeatherData(
     location.latitude,
     location.longitude
   );
 
-  // TODO: condition und image korrekt laden
+  // TODO: image korrekt laden
   // TODO: doppelte Orte vermeiden?
-  const condition = 'heiter';
+  const condition = weatherCodes.get(weathercode);
   const image =
     'https://cdn.glitch.me/c569e324-22c3-491c-ab27-94a3498d6207%2Fsun-cloudy-line.png?v=1634724998045';
   addLocation(location.name, temp, condition, image);
@@ -85,7 +85,9 @@ async function loadWeatherData(latitude, longitude) {
   const url = `${weatherURL}&latitude=${latitude}&longitude=${longitude}`;
   const data = await fetch(url);
   const json = await data.json();
-  return Math.trunc(json.current_weather.temperature);
+  const temp = Math.trunc(json.current_weather.temperature);
+  const weathercode = json.current_weather.weathercode;
+  return { temp, weathercode };
 }
 
 async function searchLocation(location) {
@@ -100,7 +102,7 @@ async function searchLocation(location) {
 search_btn.addEventListener('click', doSearch);
 add_btn.addEventListener('click', doAddFromList);
 
-let locations = [
+const locations = [
   {
     name: 'Berlin',
     latitude: 52.52,
@@ -147,6 +149,36 @@ let locations = [
     longitude: -74,
   },
 ];
+
+const weatherCodes = new Map();
+weatherCodes.set(0, 'klar');
+weatherCodes.set(1, 'überwiegend klar');
+weatherCodes.set(2, 'teilweise bewölkt');
+weatherCodes.set(3, 'zunehmend bewölkt');
+weatherCodes.set(45, 'neblig');
+weatherCodes.set(48, 'neblig mit Reif');
+weatherCodes.set(51, 'leichter Sprühregen');
+weatherCodes.set(53, 'Sprühregen');
+weatherCodes.set(55, 'starker Sprühregen');
+weatherCodes.set(56, 'leichter gefrierender Nieselregen');
+weatherCodes.set(57, 'gefrierender Nieselregen');
+weatherCodes.set(61, 'leichter Regen');
+weatherCodes.set(63, 'Regen');
+weatherCodes.set(65, 'starker Regen');
+weatherCodes.set(66, 'leichter gefrierender Regen');
+weatherCodes.set(67, 'gefrierender Regen');
+weatherCodes.set(71, 'leichter Schneefall');
+weatherCodes.set(73, 'Schneefall');
+weatherCodes.set(75, 'starker Schneefall');
+weatherCodes.set(77, 'Schneehagel');
+weatherCodes.set(80, 'leichte Regenschauer');
+weatherCodes.set(81, 'Regenschauer');
+weatherCodes.set(82, 'starker Regenschauer');
+weatherCodes.set(85, 'leichte Schneeschauer');
+weatherCodes.set(86, 'Schneeschauer');
+weatherCodes.set(95, 'leichtes Gewitter');
+weatherCodes.set(96, 'leichtes Gewitter mit Hagel');
+weatherCodes.set(99, 'Gewitter mit Hagel');
 
 function setupLocationList() {
   locations.forEach((location, index) => {
